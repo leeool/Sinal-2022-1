e8be05ae9723834e2cee4a017b6847d0a52c5cd7
leeool <lgonsalez02@gmail.com>
2022-04-01 22:26:11 -0300

    finaliza o exercicio

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿int n;
+
+Console.Write("\nDigite um número: ");
+n = Convert.ToInt32(Console.ReadLine());
+
+bool positivo = n > 0;
+bool negativo = n < 0; 
+bool zero = n == 0;
+
+if (positivo)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"{n} é POSITIVO!\n");
+
+}
+if (negativo)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"{n} é NEGATIVO!\n");
+}
+
+if (zero)
+{
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("ZERO.\n");
+}
+
+Console.ResetColor();
